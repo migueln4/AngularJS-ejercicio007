e3cc755e1583d8419a3d0c80f292d9d04e829765
migueln4 <miguel444666@gmail.com>
2018-11-21 11:42:15 +0100

    Añadido lo del tema del idioma

--- formulario-auto-2/js/app.js
+++ formulario-auto-2/js/app.js
@@ -1,17 +1,26 @@
 
-var app = angular.module('bonusApp',[ ]);
-
 var app = angular.module('bonusApp', ['jcs-autoValidate']);
 
 angular.module('jcs-autoValidate').run([
 	'defaultErrorMessageResolver',
 	function (defaultErrorMessageResolver) {
-		// To change the root resource file path
+/*
+1) Esto que se ha añadido es una forma de pode adjuntar los nuevos idiomas que se incluyen en la carpeta.
+*/
+		defaultErrorMessageResolver.setI18nFileRootPath('js/lib');
+		defaultErrorMessageResolver.setCulture('es-CO');
+/*
+2) Aquí hay que indicar dónde está el archivo donde se pueden ver las traducciones (setI18nFileRootPath).
+3) También, hay que setear la cultura para que sepa a qué archivo tiene que apuntar. En este caso, es el español de Colombia, pero se podría crear un archivo con el español de España. Ojo con las mayúsculas.
+*/
 		
 		defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
 			errorMessages['edadMinima'] = 'Debe de ser mayor a {0} años de edad.';
 			errorMessages['edadMaxima'] = 'Debe de ser menor a {0} años de edad.';
 		});
+/*
+3) .
+*/
 	}
 ]);
 
